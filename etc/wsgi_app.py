0dbff6590e043008e9d539265896d0fcdb5fc42d
Andrey S <andreysss290494@gmail.com>
2020-04-14 15:27:46 +0300

    Update wsgi_app.py

--- etc/wsgi_app.py
+++ etc/wsgi_app.py
@@ -1,6 +1,8 @@
 def wsgi_application(environ, start_response):
 	status = '200 OK'
 	headers = [('Content-type', 'text/plain')]
-	body = 'Hello, world'
+	body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
 	start_response(status, headers)
-	return [body]
+	
+	
+	return body
